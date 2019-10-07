fe91938fd977b99a0c3391a094540ac2317eab35
oladimeji-ojo <ojokuredim@gmail.com>
2019-10-07 14:39:58 +0100

    called useContext hook in Product

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useContext } from 'react';
+import ProductContext from "../Contexts/ProductContext";
+
 
 // Components
 import Product from './Product';
 
 const Products = props => {
+	const  { products, addItem }= useContext(ProductContext);
 	return (
 		<div className="products-container">
 			{props.products.map(product => (
